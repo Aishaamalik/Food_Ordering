a0a7a10b661dc5921bd51000368ebdea59808e6e
ash <113773607+Aishaamalik@users.noreply.github.com>
2024-08-19 18:40:50 +0500

    Bottom tab done

--- screens/Bottom Tab/ButtomNavigator.jsx
+++ screens/Bottom Tab/ButtomNavigator.jsx
@@ -1,7 +1,8 @@
-import { StyleSheet, View } from 'react-native';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Icon from 'react-native-vector-icons/Feather'; 
+import Icon from 'react-native-vector-icons/Feather';
+import { useSelector } from 'react-redux';
 
 // Screens
 import CartScreen from './CartScreen';
@@ -12,71 +13,78 @@ import DrawerSceneWrapper from '../Drawer/Drawer Navigation/DrawerSceneWrapper';
 
 const Bottom = createBottomTabNavigator();
 
-const ButtomNavigator = ({children}) => {
+const ButtomNavigator = () => {
+  const isDay = useSelector(state => state.theme.isDay);
+
   return (
     <DrawerSceneWrapper>
-    <Bottom.Navigator
-      screenOptions={({ route }) => ({
-        headerShown: false,
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+      <Bottom.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          switch (route.name) {
-            case 'Main':
-              iconName = 'home'; 
-              break;
-            case 'Liked':
-              iconName = 'heart'; 
-              break;
-            case 'Cart':
-              iconName = 'shopping-cart'; 
-              break;
-            case 'Profile':
-              iconName = 'user'; 
-              break;
-            default:
-              iconName = 'help-circle'; 
-              break;
-          }
+            switch (route.name) {
+              case 'Main':
+                iconName = 'home'; 
+                break;
+              case 'Liked':
+                iconName = 'heart'; 
+                break;
+              case 'Cart':
+                iconName = 'shopping-cart'; 
+                break;
+              case 'Profile':
+                iconName = 'user'; 
+                break;
+              default:
+                iconName = 'help-circle'; 
+                break;
+            }
 
-          return (
-            <View style={[
-              styles.iconContainer,
-              { backgroundColor: focused ? 'green' : 'transparent' }
-            ]}>
-              <Icon 
-                name={iconName} 
-                size={size} 
-                color={focused ? 'white' : color} 
-              />
-            </View>
-          );
-        },
-        tabBarActiveTintColor: 'green', 
-        tabBarInactiveTintColor: 'gray', 
-      })}
-    >
-      <Bottom.Screen
-        name='Main'
-        component={Main}
-        options={{ headerShown: false }}
-      />
-      <Bottom.Screen
-        name='Liked'
-        component={LikedScreen}
-        options={{ headerShown: false }}
-      />
-      <Bottom.Screen
-        name='Cart'
-        component={CartScreen}
-        options={{ headerShown: false }}
-      />
-      <Bottom.Screen
-        name='Profile'
-        component={ProfileScreen}
-        options={{ headerShown: false }}
-      />
-    </Bottom.Navigator>
+            return (
+              <View style={[
+                styles.iconContainer,
+                { 
+                  backgroundColor: focused ? (isDay ? 'green' : 'darkgreen') : 'transparent' 
+                }]
+              }>
+                <Icon 
+                  name={iconName} 
+                  size={size} 
+                  color={focused ? 'white' : color} 
+                />
+              </View>
+            );
+          },
+          tabBarActiveTintColor: isDay ? 'white' : 'lightgray', 
+          tabBarInactiveTintColor: isDay ? 'gray' : 'darkgray', 
+          tabBarStyle: {
+            backgroundColor: isDay ? '#ffffff' : 'black',
+          },
+        })}
+      >
+        <Bottom.Screen
+          name='Main'
+          component={Main}
+          options={{ headerShown: false }}
+        />
+        <Bottom.Screen
+          name='Liked'
+          component={LikedScreen}
+          options={{ headerShown: false }}
+        />
+        <Bottom.Screen
+          name='Cart'
+          component={CartScreen}
+          options={{ headerShown: false }}
+        />
+        <Bottom.Screen
+          name='Profile'
+          component={ProfileScreen}
+          options={{ headerShown: false }}
+        />
+      </Bottom.Navigator>
     </DrawerSceneWrapper>
   );
 };
